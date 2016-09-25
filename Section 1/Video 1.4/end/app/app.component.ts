0f92722604577200ca8a9325a4088c5a2f870f95
John Munsch <john.munsch@gmail.com>
2016-09-25 13:21:20 -0500

    The Video 1.4 example now works with the final release of Angular 2.

--- Section 1/Video 1.4/end/app/app.component.ts
+++ Section 1/Video 1.4/end/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
 
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// One component in our app.
 @Component({
   selector: 'app',
   template: `
@@ -16,4 +21,14 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 export class HelloWorld {
 }
 
-bootstrap(HelloWorld);
+// Create a module for our application.
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ HelloWorld ],
+  bootstrap: [ HelloWorld ]
+})
+export class AppModule { }
+
+// Bootstrap the main module.
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
