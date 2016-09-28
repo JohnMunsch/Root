40c5c2b09c8dd9fa4e6656adfd969057fb9e82c7
John Munsch <john.munsch@gmail.com>
2016-09-27 19:54:10 -0500

    Video 6.1 example works with Angular 2 final.

--- Section 6/Video 6.1/end/app/app.component.ts
+++ Section 6/Video 6.1/end/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { HTTP_PROVIDERS } from '@angular/http';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { MoviesService } from './movies.service';
 
@@ -24,4 +28,17 @@ export class AppComponent {
   }
 }
 
-bootstrap(AppComponent, HTTP_PROVIDERS);
+// Create a module for our application.
+@NgModule({
+  imports: [ BrowserModule, HttpModule ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ],
+  providers: [ MoviesService ]
+})
+export class AppModule { }
+
+// Bootstrap the main module.
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
